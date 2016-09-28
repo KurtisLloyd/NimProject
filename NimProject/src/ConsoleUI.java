6df79ec80a68b1e9139252f1a2213d3877f9709f
Justine Barry <justinianfruit@gmail.com>
2016-09-28 11:50:47 -0600

    Add files via upload

--- NimProject/src/ConsoleUI.java
+++ NimProject/src/ConsoleUI.java
@@ -52,13 +52,54 @@ public class ConsoleUI {
 		return games;
 	}
 
-	//display state of current board
-	//print values ascending left to right
-	//print a 0 if the row is empty
-	//ask which row they want
-	//ask how many to take away
-	public void printState() {
-
+	public void printState(State current) {
+		String currentState = "\n1. " + current.top + "\n2. " + current.mid + "\n3. " + current.bot;
+		System.out.println(currentState);
+		Scanner scan = new Scanner(System.in);
+		boolean validRow = false;
+		while (!validRow) {
+			System.out.println("\nSelect a row.");
+			String userRow = scan.next();
+			if (userRow.matches("\\d+(\\d+)?")) {
+				int rowNum = Integer.parseInt(userRow);
+				if (rowNum > 3 || rowNum < 1) {
+					System.out.println("Invalid entry.");
+				} else {
+					validRow = true;
+					boolean validTake = false;
+					while (!validTake) {
+						System.out.println("How many do you wish to subtract?");
+						String userTake = scan.next();
+						if (userTake.matches("\\d+(\\d+)?")) {
+							int takeNum = Integer.parseInt(userTake);
+							if (rowNum == 1) {
+								if (current.top < takeNum) {
+									System.out.println("The row doesn't not contain that value.");
+								} else {
+									validTake = true;
+								}
+							} else if (rowNum == 2) {
+								if (current.mid < takeNum) {
+									System.out.println("The row doesn't not contain that value.");
+								} else {
+									validTake = true;
+								}
+							} else {
+								if (current.bot < takeNum) {
+									System.out.println("The row doesn't not contain that value.");
+								} else {
+									validTake = true;
+								}
+							}
+						} else {
+							System.out.println("Please enter a number.");
+						}
+					}
+				}
+			} else {
+				System.out.println("Please enter a number.");
+			}
+		}
+		//pass row number and number to take away to game
 	}
-
 }
